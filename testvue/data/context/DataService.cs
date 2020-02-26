fef83d166374b3199897c01fff967791a3e29cda
fabiancaperan <38954803+fabiancaperan@users.noreply.github.com>
2020-02-25 19:38:14 -0500

    aadsfdsaf
    asdfdsaf

---
+++ testvue/data/context/DataService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testvue.data.model;
+
+namespace testvue.data.context
+{
+    public class DataService:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+        {            
+            options.UseOracle(@"User Id=dummy;Password=dummy;Data Source=localhost:1521/ORCLCDB.localdomain");
+        }
+    }
+}
---
+++ testvue/data/model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testvue.data.model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
